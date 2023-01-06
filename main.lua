425cef35c38dce9a4486c65ad30284324aa636ab
ThiagoLRG <54580798+ThiagoLRG@users.noreply.github.com>
2023-01-06 19:05:53 -0300

    added colored tiles, moved code out of main, added example of usage in main, better comments and readme

--- main.lua
+++ main.lua
@@ -1,69 +1,24 @@
 function love.load()
-    
-    -----------------display, aspectratio, resolution stuff-----------------
-    --[[
-        I countend in this old DOS game printscreen 40x25 (WxH) tiles, each tile with 8 pixels
-        This would result in an 8:5 aspect ratio if tiles we're squares.
-        Pixels we're vertically streched to fit 4:3 (8:6) aspect ratio. in the printscreen 1600:1200 resolution pixels are 40x48
-        I never knew pixels we're vertically streched before this
-        https://www.neogaf.com/threads/ms-dos-games-and-aspect-ratio.1108226/
-    --]]
-
-    --original scale in pixels and aspectratio to display
-    pixelsInTileWidth = 8
-    pixelsInTileHeight = 8
-    tilesInWidth = 40
-    tilesInHeight = 25
-    aspectRatioWidth = 4
-    aspectRatioHeight = 3
-
-    --game in fullscreen and width and height of display
-    love.window.setMode(0,0,{fullscreen= true})
-    displayWidth, displayHeight = love.window.getMode()
-
-    --make best defined aspect ratio aproximation of current display resolution
-    widthCentralization = 0
-    heightCentralization = 0
-    gameDisplayWidth = displayWidth
-    gameDisplayHeight = displayHeight
-    if displayWidth / displayHeight > aspectRatioWidth / aspectRatioHeight then
-        gameDisplayWidth = displayHeight * aspectRatioWidth / aspectRatioHeight
-        widthCentralization = (displayWidth - gameDisplayWidth) / 2
-
-    elseif displayWidth / displayHeight < aspectRatioWidth / aspectRatioHeight then
-        gameDisplayHeight = displayWidth * aspectRatioHeight / aspectRatioWidth
-        heightCentralization = (displayHeight - gameDisplayHeight) / 2
-    end
-    
-    --the exact pixels and tiles is streched to fit a defined aspect ratio aproximation in any resolution and aspect ratio
-    titleScaledWidthPx = gameDisplayWidth / tilesInWidth
-    titleScaledHeightPx = gameDisplayHeight / tilesInHeight
-    pixelScaledWidthPx = titleScaledWidthPx / pixelsInTileWidth
-    pixelScaledHeightPx = titleScaledHeightPx / pixelsInTileHeight
-
-    -----------------tiles avaliable-----------------
     require('tiles')
 
-    
-    -----------------level logic-----------------
-    cycles = diagonalsCycles(6)
-    userPath = {0, 2, 5, 1}
-    highlightedCycle = 2
+    --window size to fullscreen and get windowWidth, windowHeight
+    love.window.setMode(0,0,{fullscreen= true})
+    windowWidth, windowHeight = love.window.getMode()
 
+    --these are the default values, don't have to be defined, you can change if you want
+    tilesDefinitions.tilesInWidth = 40
+    tilesDefinitions.tilesInHeight = 25
+    tilesDefinitions.aspectRatioWidth = 4
+    tilesDefinitions.aspectRatioHeight = 3
+
+    --call this function to ajust game resolution to best fit current window size
+    gameResolutioninWindow(windowWidth, windowHeight)    
 end
 
 function love.draw()
-end
-
-function drawTile(tile, xLeft, yTop)
-    for  yPixel, row in ipairs(tile) do
-        for xPixel, isToDraw in ipairs(row) do
-            if isToDraw == 1 then
-                love.graphics.rectangle('fill',
-                    (xLeft * titleScaledWidthPx) + (pixelScaledWidthPx * (xPixel - 1) + widthCentralization),
-                    (yTop * titleScaledHeightPx) + (pixelScaledHeightPx * (yPixel - 1) + heightCentralization),
-                    pixelScaledWidthPx, pixelScaledHeightPx)
-            end
+    for yPosition = 0, tilesDefinitions.tilesInHeight - 1, 1 do
+        for xPosition = 0, tilesDefinitions.tilesInWidth - 1, 1 do
+            drawTile(tiles.colorTest, xPosition, yPosition)
         end
     end
 end
--- tiles.lua
+++ tiles.lua
@@ -1,5 +1,6 @@
+--tiles types, each tile is 8x8 in game pixels, this has to corresponde with tilesDefinitions.pixelsInTileWidth and tilesDefinitions.pixelsInTileHeight
 tiles = {
-    empty = {
+    dontDraw = {
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
@@ -9,104 +10,83 @@ tiles = {
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
     },
-    [0] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 1, 0, 0, 1, 0, 1, 0},
-        {0, 1, 0, 1, 0, 0, 1, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
+    box = {
+        {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
+        {'w', 0, 0, 0, 0, 0, 0, 'w'},
+        {'w', 0, 0, 0, 0, 0, 0, 'w'},
+        {'w', 0, 0, 0, 0, 0, 0, 'w'},
+        {'w', 0, 0, 0, 0, 0, 0, 'w'},
+        {'w', 0, 0, 0, 0, 0, 0, 'w'},
+        {'w', 0, 0, 0, 0, 0, 0, 'w'},
+        {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
     },
-    [1] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 1, 1, 0, 0, 0},
-        {0, 0, 1, 1, 1, 0, 0, 0},
-        {0, 1, 1, 1, 1, 0, 0, 0},
-        {0, 0, 0, 1, 1, 0, 0, 0},
-        {0, 0, 0, 1, 1, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 1, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [2] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 0, 0, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 0, 0, 0},
-        {0, 1, 1, 0, 0, 0, 1, 0},
-        {0, 1, 1, 1, 1, 1, 1, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [3] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 0, 0, 0, 1, 1, 0},
-        {0, 0, 0, 0, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [4] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 1, 1, 0, 0},
-        {0, 1, 1, 1, 1, 1, 1, 0},
-        {0, 0, 0, 0, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [5] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 1, 0},
-        {0, 1, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 1, 0},
-        {0, 0, 0, 0, 0, 0, 1, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [6] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 1, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [7] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 1, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 0, 0, 1, 1, 0, 0},
-        {0, 0, 0, 1, 1, 0, 0, 0},
-        {0, 0, 1, 1, 0, 0, 0, 0},
-        {0, 0, 1, 1, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [8] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-    },
-    [9] = {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 1, 0},
-        {0, 0, 0, 0, 0, 0, 1, 0},
-        {0, 1, 1, 0, 0, 1, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
+    colorTest = {
+        {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
+        {'w', 'r', 'r', 'g', 'g', 'b', 'b', 'w'},
+        {'w', 'r', 'r', 'g', 'g', 'b', 'b', 'w'},
+        {'w', 'r', 'r', 'g', 'g', 'b', 'b', 'w'},
+        {'w', 'r', 'r', 'g', 'g', 'b', 'b', 'w'},
+        {'w', 'r', 'r', 'g', 'g', 'b', 'b', 'w'},
+        {'w', 'r', 'r', 'g', 'g', 'b', 'b', 'w'},
+        {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
     },
 }
+
+--color types used in tiles pixels, you can change names to numbers to generate and modify tiles through programming 
+colors = {
+    w = {255, 255, 255, 1},
+    r = {255, 0, 0, 1},
+    g = {0, 255, 0, 1},
+    b = {0, 0, 255, 1},
+}
+
+--default game definitions
+tilesDefinitions = {
+    pixelsInTileWidth = 8,
+    pixelsInTileHeight = 8,
+    tilesInWidth = 40,
+    tilesInHeight = 25,
+    aspectRatioWidth = 4,
+    aspectRatioHeight = 3,
+}
+
+--ajust game to current window size, you have to call this in main with current window size
+tilesResolution = {}
+function gameResolutioninWindow(windowWidth, windowHeight)
+    tilesResolution.widthCenter = 0
+    tilesResolution.heightCcenter = 0
+    tilesResolution.width = windowWidth
+    tilesResolution.height = windowHeight
+    --make best aproximation of game aspect ratio to fit in current monitor resolution
+    if windowWidth / windowHeight > tilesDefinitions.aspectRatioWidth / tilesDefinitions.aspectRatioHeight then
+        tilesResolution.width = windowHeight * tilesDefinitions.aspectRatioWidth / tilesDefinitions.aspectRatioHeight
+        tilesResolution.widthCenter = (windowWidth - tilesResolution.width) / 2
+    elseif windowWidth / windowHeight < tilesDefinitions.aspectRatioWidth / tilesDefinitions.aspectRatioHeight then
+        tilesResolution.height = windowWidth * tilesDefinitions.aspectRatioHeight / tilesDefinitions.aspectRatioWidth
+        tilesResolution.heightCcenter = (windowHeight - tilesResolution.height) / 2
+    end
+
+    --the exact size of game tile and game tile pixel in monitor pixels
+    tilesResolution.tileWidth = tilesResolution.width / tilesDefinitions.tilesInWidth
+    tilesResolution.tileHeight = tilesResolution.height / tilesDefinitions.tilesInHeight
+    tilesResolution.tilePixelWidth = tilesResolution.tileWidth / tilesDefinitions.pixelsInTileWidth
+    tilesResolution.tilePixelHeight = tilesResolution.tileHeight / tilesDefinitions.pixelsInTileHeight
+end
+
+--Draw tile, tile type, position in x and with. Position is in number of tiles. Ex. 40 tiles in width xPosition => 0 < 40 
+function drawTile(tile, xPosition, yPosition)
+    if xPosition < tilesDefinitions.tilesInWidth and yPosition < tilesDefinitions.tilesInHeight then
+        for  yTilePixel, row in ipairs(tile) do
+            for xTilePixel, tilePixelColorReference in ipairs(row) do
+                if tilePixelColorReference ~= 0 then
+                    color = colors[tilePixelColorReference]
+                    love.graphics.setColor(color[1], color[2], color[3], color[4])
+                    love.graphics.rectangle('fill',
+                        (xPosition * tilesResolution.tileWidth) + (tilesResolution.tilePixelWidth * (xTilePixel - 1) + tilesResolution.widthCenter),
+                        (yPosition * tilesResolution.tileHeight) + (tilesResolution.tilePixelHeight * (yTilePixel - 1) + tilesResolution.heightCcenter),
+                        tilesResolution.tilePixelWidth, tilesResolution.tilePixelHeight)
+                end
+            end
+        end
+    end
+end
